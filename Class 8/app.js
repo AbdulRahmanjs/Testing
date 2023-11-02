96f61d819334af34d25c92a24aff08d70e3218e4
AbdulRahmanjs <abduljs9292@gmail.com>
2023-11-03 03:17:16 +0500

    new folder

---
+++ Class 8/app.js
@@ -0,0 +1,36 @@
+// function abc(){
+// alert('Hello World')
+// }
+
+// for(i=1;i<=10;i++){
+
+//     abc()
+// }
+
+// function sum(a=1,b=2,c=3){
+
+//     return a+b+c
+// }
+
+// alert(sum(7,7,7))
+
+
+function sum(a=1,b=2,c=3){
+
+    
+var t=+prompt('Enter Value 1')
+var t1=+prompt('Enter Value 2')
+var t2=+prompt('Enter Value 3')
+
+if(t==""&& t1==""&& t2==""){
+return a+b+c
+}
+else{
+   return t+t1+t2
+}
+
+}
+
+alert(sum())
+
+
